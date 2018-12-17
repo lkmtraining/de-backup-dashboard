b7e5df994e41ad0bb649a315ca2a755e6fe4c5a7
lochanm2 <lochan.marwah@springernature.com>
2018-12-17 20:31:55 +0530

    7

--- bonbackup/settings.py
+++ bonbackup/settings.py
@@ -132,9 +132,10 @@ USE_TZ = True
 
 #STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, 'static'),
-)
 
 STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, 'static'),
+)
